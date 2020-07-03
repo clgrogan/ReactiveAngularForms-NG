6faaca7c512520444845b0ee7352652a425c22c6
Curt Grogan <curtgrogan@outlook.com>
2020-07-03 00:50:46 -0400

    Moved username.validators to common dir.

--- src/app/common/validators/username.validators.ts
+++ src/app/common/validators/username.validators.ts
@@ -4,7 +4,7 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
     // *.value is type any, but we want to handle as string
     if ((control.value as string).indexOf(" ") !== -1) {
-      return { cannotContainSpace: true };
+      return { cannotContainSpace: true }; // key: value pair. key is a string value is type any, we returned bool.
     } else {
       return null;
     }
