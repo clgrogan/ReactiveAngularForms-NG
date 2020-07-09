b3e2b04a36e08f9a224912d56a41e1c808230fb5
Curt Grogan <curtgrogan@outlook.com>
2020-07-09 13:35:13 -0400

    Nested Form Groups

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -8,22 +8,34 @@ import { UsernameValidators } from "../common/validators/username.validators";
 })
 export class LoginFormComponent {
   deForm = new FormGroup({
-    username: new FormControl(
-      "",
-      [
-        Validators.required,
-        Validators.minLength(3),
-        UsernameValidators.cannotContainSpace,
-      ],
-      UsernameValidators.mustBeUnique
-    ),
+    account: new FormGroup({
+      username: new FormControl(
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace,
+        ],
+        UsernameValidators.mustBeUnique
+      ),
+    }),
     password: new FormControl("", Validators.required),
   });
+
+  login() {
+    // let isValid = authService.login(this.deForm.value);
+    const isValid = false;
+    if (!isValid) {
+      this.deForm.setErrors({
+        invalidLogin: true,
+      });
+    }
+  }
   // We can create properties for the class instance to give access to the form control object properties and their attributes/methods
   get username() {
-    return this.deForm.get("username");
+    return this.deForm.get("account.username"); //username is nested in the account FormGroup.
   }
   get password() {
-    return this.deForm.get("password");
+    return this.deForm.get("password"); //password is directly under deForm
   }
 }
